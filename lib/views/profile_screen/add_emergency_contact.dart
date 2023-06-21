78eb09b3a985edc8fcd7d30f8f08f80e0b175a08
mahmudebnezaman <mahmudebnejaman@gmail.com>
2023-06-21 11:00:11 +0600

    profile settings updated

--- lib/views/profile_screen/add_emergency_contact.dart
+++ lib/views/profile_screen/add_emergency_contact.dart
@@ -15,75 +15,76 @@ class EmergencyContact extends StatefulWidget {
 }
 
 class _EmergencyContactState extends State<EmergencyContact> {
-  
+  late ProfileController controller;
 
-  bool isPass = true;
-
-  void togglePasswordView() {
-    setState(() {
-      isPass = !isPass;
-    });
+  @override
+  void initState() {
+    super.initState();
+    controller = Get.find<ProfileController>();
+    controller.emergencycontactController.text = widget.data['emergency_contact'];
+    controller.emergencycontactNameController.text = widget.data['emergency_contact_name'];
   }
 
   @override
   Widget build(BuildContext context) {
-    var controller = Get.find<ProfileController>();
 
-    controller.emergencycontactController.text = widget.data['emergency_contact'];
-    controller.emergencycontactNameController.text = widget.data['emergency_contact_name'];
-
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: "Emergency Contact".text.make(),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Obx(
-            ()=> Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                customTextFeild(
-                  title: name,
-                  hint: "Given Name",
-                  prefixIcon: const Icon(Icons.account_circle_outlined, size: 25,),
-                  controller: controller.emergencycontactNameController,
-                ),
-
-                10.heightBox,
-                
-                customTextFeild(
-                  title: "Emergency Contact",
-                  hint: "e.g. 012345678912",
-                  prefixIcon: const Icon(Icons.mobile_friendly_outlined),
-                  controller: controller.emergencycontactController,
-                  keytype: TextInputType.number,
-                  maxLength: 11
-                ),
-                
-                const Spacer(),
-                controller.isloading.value ? loadingIndicator() : SizedBox(
-                  width: context.screenWidth-40,
-                  child: myButton(
-                    color: primary,
-                    buttonSize: 20.0,
-                    onPress: ()async{
-                      controller.isloading(true);
-                      await controller.updateEmergencyContact(
-                        contact: controller.emergencycontactController.text,
-                        name: controller.emergencycontactNameController.text
-                      );
-                      VxToast.show(context, msg: 'Saved');
-                   },
-                    textColor: whiteColor,
-                    title: 'Save',
+    return WillPopScope(
+      onWillPop: () async {
+        controller.reset();
+        return true;
+      },
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: "Emergency Contact".text.make(),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Obx(
+              ()=> Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  customTextFeild(
+                    title: name,
+                    hint: "Given Name",
+                    prefixIcon: const Icon(Icons.account_circle_outlined, size: 25,),
+                    controller: controller.emergencycontactNameController,
                   ),
-                ),
-                
-              ],
+    
+                  10.heightBox,
+                  
+                  customTextFeild(
+                    title: "Emergency Contact",
+                    hint: "e.g. 012345678912",
+                    prefixIcon: const Icon(Icons.mobile_friendly_outlined),
+                    controller: controller.emergencycontactController,
+                    keytype: TextInputType.number,
+                    maxLength: 11
+                  ),
+                  
+                  const Spacer(),
+                  controller.isloading.value ? loadingIndicator() : SizedBox(
+                    width: context.screenWidth-40,
+                    child: myButton(
+                      color: primary,
+                      buttonSize: 20.0,
+                      onPress: ()async{
+                        controller.isloading(true);
+                        await controller.updateEmergencyContact(
+                          contact: controller.emergencycontactController.text,
+                          name: controller.emergencycontactNameController.text
+                        );
+                        VxToast.show(context, msg: 'Saved');
+                     },
+                      textColor: whiteColor,
+                      title: 'Save',
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      );
+    );
   }
 }
--- lib/views/profile_screen/edit_profile.dart
+++ lib/views/profile_screen/edit_profile.dart
@@ -34,82 +34,88 @@ class _EditProfileInfoState extends State<EditProfileInfo> {
     var nameIcon = const Icon(Icons.account_circle_outlined, size: 25,);
     var controller = Get.find<ProfileController>();
 
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Obx(
-            ()=> Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Stack(
-                    children: [
-                      widget.data['imageUrl'] == '' && controller.profileImagePath.isEmpty? Image.asset(icUser,fit: BoxFit.cover,height: 100,width: 100, color: fontGrey,).box.clip(Clip.antiAlias).roundedFull.white.shadow3xl.make()
-                      : widget.data['imageUrl'] != '' && controller.profileImagePath.isEmpty? Image.network(widget.data['imageUrl'],fit: BoxFit.cover,height: 100,width: 100).box.clip(Clip.antiAlias).roundedFull.border(color: whiteColor, width: 2).white.shadow3xl.make()
-                      : Image.file(File(controller.profileImagePath.value), width: 100, fit: BoxFit.cover,).box.clip(Clip.antiAlias).roundedFull.border(color: whiteColor, width: 2).white.shadow3xl.make(),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Image.asset(icPencil,fit: BoxFit.fill,color: highEmphasis,height: 15,).box.roundedFull.white.padding(const EdgeInsets.all(4)).shadowSm.make().onTap(() {
-                    controller.changeImage(context);})),
-
-                    ],
-                  ).box.height(100).width(100).make(),
-                const Divider(),
-                20.heightBox,
-                customTextFeild(
-                  controller: controller.nameController,
-                  hint: nameHint,
-                  title: name,
-                  prefixIcon: nameIcon,
-                ),
-                10.heightBox,
-                customPasswordFeild(hint: passwordHint, title: password, obsText: isPass, suffixIcon: InkWell(
-                    onTap: togglePasswordView,
-                    child: Icon(
-                      isPass 
-                        ? Icons.visibility 
-                        : Icons.visibility_off,
-                    )), controller: controller.passController),
-                
-                const Spacer(),
-                controller.isloading.value ? loadingIndicator() : SizedBox(
-                  width: context.screenWidth-40,
-                  child: myButton(
-                    color: primary,
-                    buttonSize: 20.0,
-                    onPress: ()async{
-          
-                      controller.isloading(true);
-          
-                      //if image is not selected
-                      if(controller.profileImagePath.value.isNotEmpty){
-                        await controller.uploadProfileImage();
-                      } else {
-                        controller.profileImageLink = widget.data['imageUrl'];
-                      }
-          
-                      //if old password matches database
-                      if(widget.data['password'] == controller.passController.text){
-                        await controller.updateProfile(
-                        imgUrl: controller.profileImageLink,
-                        name: controller.nameController.text,
-                        );
-                        VxToast.show(context, msg: 'Updated');
-                      } else{
-                        VxToast.show(context, msg: 'Password doesnot matched with old password');
-                        controller.isloading(false);
-                      }
-                   },
-                    textColor: whiteColor,
-                    title: 'Save',
+    return WillPopScope(
+      onWillPop: () async {
+        controller.reset();
+        return true;
+      },
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(),
+          body: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Obx(
+              ()=> Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Stack(
+                      children: [
+                        widget.data['imageUrl'] == '' && controller.profileImagePath.isEmpty? Image.asset(icUser,fit: BoxFit.cover,height: 100,width: 100, color: fontGrey,).box.clip(Clip.antiAlias).roundedFull.white.shadow3xl.make()
+                        : widget.data['imageUrl'] != '' && controller.profileImagePath.isEmpty? Image.network(widget.data['imageUrl'],fit: BoxFit.cover,height: 100,width: 100).box.clip(Clip.antiAlias).roundedFull.border(color: whiteColor, width: 2).white.shadow3xl.make()
+                        : Image.file(File(controller.profileImagePath.value), width: 100, fit: BoxFit.cover,).box.clip(Clip.antiAlias).roundedFull.border(color: whiteColor, width: 2).white.shadow3xl.make(),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Image.asset(icPencil,fit: BoxFit.fill,color: highEmphasis,height: 15,).box.roundedFull.white.padding(const EdgeInsets.all(4)).shadowSm.make().onTap(() {
+                      controller.changeImage(context);})),
+    
+                      ],
+                    ).box.height(100).width(100).make(),
+                  const Divider(),
+                  20.heightBox,
+                  customTextFeild(
+                    controller: controller.nameController,
+                    hint: nameHint,
+                    title: name,
+                    prefixIcon: nameIcon,
                   ),
-                ),
-                
-              ],
+                  10.heightBox,
+                  customPasswordFeild(hint: passwordHint, title: password, obsText: isPass, suffixIcon: InkWell(
+                      onTap: togglePasswordView,
+                      child: Icon(
+                        isPass 
+                          ? Icons.visibility 
+                          : Icons.visibility_off,
+                      )), controller: controller.passController),
+                  
+                  const Spacer(),
+                  controller.isloading.value ? loadingIndicator() : SizedBox(
+                    width: context.screenWidth-40,
+                    child: myButton(
+                      color: primary,
+                      buttonSize: 20.0,
+                      onPress: ()async{
+            
+                        controller.isloading(true);
+            
+                        //if image is not selected
+                        if(controller.profileImagePath.value.isNotEmpty){
+                          await controller.uploadProfileImage();
+                        } else {
+                          controller.profileImageLink = widget.data['imageUrl'];
+                        }
+            
+                        //if old password matches database
+                        if(widget.data['password'] == controller.passController.text){
+                          await controller.updateProfile(
+                          imgUrl: controller.profileImageLink,
+                          name: controller.nameController.text,
+                          );
+                          VxToast.show(context, msg: 'Updated');
+                        } else{
+                          VxToast.show(context, msg: 'Password doesnot matched with old password');
+                          controller.isloading(false);
+                        }
+                     },
+                      textColor: whiteColor,
+                      title: 'Save',
+                    ),
+                  ),
+                  
+                ],
+              ),
             ),
           ),
         ),
-      );
+    );
   }
 }
